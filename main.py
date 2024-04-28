0a90ea53e39d54c8447b004ddfe5c5ab389670ee
Hardik <202001051.hardikjlm@student.xavier.ac.in>
2024-04-28 23:17:08 +0530

    none

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-import asyncio
-from process_video import trim_video
-from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton,Bot
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters, CallbackQueryHandler
 from utis import is_valid_youtube_link
-from process_link import get_available_qualities, download_video
 import uuid
+import subprocess
 import json
-
 
 """
 token: Unique ID of telegram Bot
@@ -15,6 +12,7 @@ dir_path:  dir where downloaded videos will be stored
 with open('config.json') as f:
     config = json.load(f)
     dir_path = config['videos_path']
+    print(dir_path)
     token = config['token']
 
 """
@@ -29,19 +27,12 @@ state = {5343698850: {
 }}
 
 
-async def trim_and_send(user_id: int, chat_id: int):
-    start = state[user_id]['start_timestamp']
-    end = state[user_id]['end_timestamp']
-    filename = await state[user_id]['filename']
-    trim_video(filename, start, end)
-    state[user_id]['step'] = None
-    await Bot.send_video(chat_id, video=dir_path+'trimmed-'+filename)
 
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """
     This function has been registered as callback for /start command handler.
-    :param update:contains all the information about the user query which made this callback execute
+    update:contains all the information about the user query which made this callback execute
     """
     user_id = update.message.from_user.id
     if user_id not in state.keys():
@@ -55,49 +46,69 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         f'Hey {update.effective_user.first_name}, Please send the link of YouTube Video you want to trim')
 
 
+def check_timestamp(timestamp):
+    timestamp = timestamp.split('.')
+    if len(timestamp) > 3:
+        return None
+    for i in timestamp:
+        if len(i) > 2 or not i.isdigit():
+            return None
+    return ':'.join(timestamp)
+
+def get_timestamp(text):
+    if len(text)<3:
+        start = 0
+    else:
+        start = check_timestamp(text[1])
+    end = check_timestamp(text[-1])
+    return start,end
+
+
+import os
+
+
+def find_file_with_prefix( prefix):
+    # Get list of files in the directory
+    files = os.listdir(dir_path)
+
+    # Filter files that start with the given prefix
+    matching_files = [file for file in files if file.startswith(prefix)]
+
+    return matching_files[0]
+
+
+
+
 async def handle_messages(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user_id = update.message.from_user.id
     if user_id not in state.keys():
         return
-    text = update.message.text
+    text = update.message.text.split(' ')
+    link = text[0]
     if state[user_id]['step'] is None:
-        if not is_valid_youtube_link(text):
+        if not is_valid_youtube_link(text[0]):
             await update.message.reply_text('Send a valid YouTube Video URL.')
         else:
-            state[user_id]['filename'] = asyncio.create_task(download_video(youtube_link=text,filename=str(uuid.uuid4())[:8]+'.mp4'))
-            state[user_id]['step'] = 'start_time'
-            await update.message.reply_text('Enter the start time')
-            # videos = get_available_qualities(text)
-            # buttons = []
-            # for video in videos:
-            #     if video.resolution:
-            #         buttons.append([InlineKeyboardButton(text=f"⬇️{video.resolution}, ~{video.filesize_mb:.2f}MB",
-            #                                              callback_data=f'download|{video.itag}')])
-            # inline_buttons = InlineKeyboardMarkup(buttons)
-            # # print(inline_buttons)
-            # state[user_id]['link'] = text
-            # await update.message.reply_text(text='Select the desired video quality.', reply_markup=inline_buttons)
-    elif state[user_id]['step'] == 'start_time':
-        state[user_id]['start_timestamp'] = int(update.message.text)
-        await update.message.reply_text('Enter the end time')
-        state[user_id]['step'] = 'end_time'
-    elif state[user_id]['step'] == 'end_time':
-        state[user_id]['start_timestamp'] = int(update.message.text)
-        chat_id = update.message.chat_id
-        asyncio.create_task(trim_and_send(user_id, chat_id))
+            start,end = get_timestamp(text)
+            if not end:
+                await update.message.reply_text('Send a valid timestamp range')
+            else:
+                filename = str(uuid.uuid4())[:5]
+                output_file = dir_path+filename
+                cmd = f'yt-dlp --download-sections "*{start}-{end}" --force-keyframes-at-cuts -o {output_file} {link}'
+                print(cmd)
+                subprocess.run(cmd)
+                output_file = dir_path + find_file_with_prefix(filename)
+                print(output_file)
+                await update.message.reply_video(video=output_file,read_timeout=600,write_timeout=600)
 
-
-# async def get_video(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     user_id = update.effective_user.id
-#     data = update.callback_query.data
-#     itag = data.split('|')[1]
-#     youtube_link = state[user_id]['link']
-#     task = asyncio.create_task(download_video(youtube_link, itag))
-#     state[user_id]['path'] = task
-#     await update.callback_query.message.delete()
-#     print(state)
-#     await update.callback_query.message.reply_text('Enter the start time')
-
+                from pathlib import Path
+                file_path = Path(output_file)
+                if file_path.exists():
+                    file_path.unlink()
+                    print("File deleted successfully.")
+                else:
+                    print("File does not exist.")
 
 
 # Builds the server side application for our telegram bot using the bot token
