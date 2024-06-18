2ade931d9e17aa1768195f8a5b56109b30c69cc4
warlock1729 <139436663+warlock1729@users.noreply.github.com>
2024-06-19 00:14:06 +0530

    Update utis.py
    Added patterns to handle additional cases

--- utis.py
+++ utis.py
@@ -28,14 +28,25 @@ import re
 def parse_timestamp(timestamp):
     patterns = [
         r'(\d{1,2})h\s*(\d{1,2})m\s*(\d{1,2})s',  # hh mm ss
-        r'(\d{1,2})\.(\d{1,2})\.(\d{2})',  # hh.mm.ss
-        r'(\d{1,2}):(\d{1,2}):(\d{2})'  # hh:mm:ss
+        r'(\d{1,2})\.(\d{1,2})\.(\d{1,2})',  # hh.mm.ss
+        r'(\d{1,2}):(\d{1,2}):(\d{1,2})',  # hh:mm:ss
+        r'(\d{1,2})\.(\d{1,2})', # mm.ss
+        r'(\d{1,2})\:(\d{1,2})', # mm:ss
+        r'(\d{1,2})' # ss
+
     ]
 
     for pattern in patterns:
         match = re.match(pattern, timestamp)
+        hours = minutes = seconds = 0
         if match:
-            hours, minutes, seconds = map(int, match.groups())
+            groups = match.groups()
+            if len(groups) == 3:
+                hours, minutes, seconds = map(int, groups)
+            elif len(groups) == 2:
+                minutes, seconds = map(int, groups)
+            elif len(groups) == 1:
+                return int(groups[0])
             return (hours * 3600) + (minutes * 60) + seconds
 
     return None
