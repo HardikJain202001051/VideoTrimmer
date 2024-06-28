6082ba4c24f9c5c453f2524d05144f8785912b62
Hardik <202001051.hardikjlm@student.xavier.ac.in>
2024-06-28 13:27:17 +0530

    regex updated

--- utis.py
+++ utis.py
@@ -17,7 +17,7 @@ class Config:
 
 def is_valid_youtube_link(url):
     # Regular expression to match YouTube URLs
-    pattern = r'https?://(?:www\.)?(?:youtube\.com/watch\?v=|youtu\.be/)[\w-]+'
+    pattern = r'https?://(?:www\.)?(?:youtube\.com/|youtu\.be/).+'
     # Check if the URL matches the pattern
     if re.match(pattern, url):
         return True
